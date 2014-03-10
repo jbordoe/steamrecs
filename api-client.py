aa46cad8a2d87304b5fef892481112838ddfb3d4
Soheb <soheby@gmail.com>
2014-03-10 21:58:04 +0000

    broke off the cli from the api backend

---
+++ api-client.py
@@ -0,0 +1,24 @@
+__author__ = 'soheb'
+
+import steamapi
+import argparse
+
+# somoso's profile
+defaultSteamID = 76561197983149697
+
+parser = argparse.ArgumentParser(description="Steam API implemented in Python")
+parser.add_argument('--steamID', dest='steamID', help="The steam ID in 64 bit format")
+argument = parser.parse_args()
+
+steamID = defaultSteamID
+if argument.steamID:
+    steamID = argument.steamID
+
+key = steamapi.getsteamkey()
+
+print("Fetching data about user " + steamID)
+result = steamapi.getownedgames(key, steamID)
+if result is None:
+    print("Failure in getting data, probably because of dud steam ID")
+else:
+    print(steamapi.getownedgames(key, steamID))
--- steamapi.py
+++ steamapi.py
@@ -4,26 +4,10 @@ __author__ = 'soheb'
 # http://legacy.python.org/dev/peps/pep-0257/
 
 import urllib.request
-import argparse
+
 import json
 
-# somoso's profile
-defaultSteamID = 76561197983149697
-
-def main():
-    parser = argparse.ArgumentParser(description="Steam API implemented in Python")
-    parser.add_argument('--steamID', dest='steamID', help="uses provided steam ID instead of default one")
-    argument = parser.parse_args()
-
-    steamID = defaultSteamID
-    if argument.steamID:
-        steamID = argument.steamID
-
-    key = getKey()
-
-    print(getOwnedGames(key, steamID))
-
-def getOwnedGames(key, steamID):
+def getownedgames(key, steamID):
     """ Return parsed JSON data of all owned games by the steam ID """
     url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + str(key) + "&steamid=" + str(steamID) + "&include_appinfo=1&include_played_free_games=1"
     try:
@@ -37,12 +21,9 @@ def getOwnedGames(key, steamID):
         if "500 Internal Server Error" in data:
             return None
 
-def getKey():
+def getsteamkey():
     """ Return the steam key found in the same directory as this code """
     steamKeyFile = open('steamkey', 'r')
     key = steamKeyFile.readline()
     steamKeyFile.close()
     return key.rstrip('\n')
-
-if __name__ == "__main__":
-    main()
