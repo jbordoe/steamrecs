bd744247b8ed0b5354c0a2fc9edf67db6ee35af9
Soheb <soheby@gmail.com>
2014-03-10 22:01:31 +0000

    added unit testing

---
+++ steamapitest.py
@@ -0,0 +1,21 @@
+__author__ = 'soheb'
+
+import unittest
+import steamapi
+
+
+class MyTestCase(unittest.TestCase):
+    def test_fake_user(self):
+        key = steamapi.getsteamkey()
+        self.assertIsNone(steamapi.getownedgames(key, 0))
+
+    def test_fake_key(self):
+        self.assertIsNone(steamapi.getownedgames(0,76561197983149697))
+
+    def test_success(self):
+        key = steamapi.getsteamkey()
+        self.assertIsNotNone(steamapi.getownedgames(key, 76561197983149697))
+
+
+if __name__ == '__main__':
+    unittest.main()
