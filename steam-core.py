0d924485a71ea266381a921fe998ae1c30b7f407
Soheb <soheby@gmail.com>
2014-03-10 21:50:11 +0000

    improved api to make it more usable when using in other scripts

--- steam-core.py
+++ steam-core.py
@@ -3,7 +3,9 @@ __author__ = 'soheb'
 # For good documentation procedure when commenting
 # http://legacy.python.org/dev/peps/pep-0257/
 
-import urllib.request, argparse
+import urllib.request
+import argparse
+import json
 
 # somoso's profile
 defaultSteamID = 76561197983149697
@@ -19,22 +21,21 @@ def main():
 
     key = getKey()
 
-    getOwnedGames(key, steamID)
+    print(getOwnedGames(key, steamID))
 
 def getOwnedGames(key, steamID):
     """ Return parsed JSON data of all owned games by the steam ID """
     url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + str(key) + "&steamid=" + str(steamID) + "&include_appinfo=1&include_played_free_games=1"
     try:
         response = urllib.request.urlopen(url)
-        data = response.read().decode('utf-8')
-        print(data)
+        # unfortunately we cannot just do json.load(response) because urllib is stupid
+        # http://stackoverflow.com/a/6862922/617028
+        str_response = response.readall().decode('utf-8')
+        return json.loads(str_response)
     except urllib.request.HTTPError as error:
         data = error.read().decode('utf-8')
         if "500 Internal Server Error" in data:
-            print("Failed to retrieve data as steam ID is probably bogus")
-
-
-
+            return None
 
 def getKey():
     """ Return the steam key found in the same directory as this code """
